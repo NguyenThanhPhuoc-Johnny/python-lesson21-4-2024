98dc801b0826621520eadc01cc157e3d528f7c99
NguyenThanhPhuoc-Johnny <164726936+NguyenThanhPhuoc-Johnny@users.noreply.github.com>
2024-04-21 15:06:28 +0700

    Update bai4.py

--- bai4.py
+++ bai4.py
@@ -1,8 +1,22 @@
 answer = "Correct"
 while answer:
-    answer = input("When is the first computer invented?: \n A: 1822 \n B: 1975 \n C: 1834 \n D: 1823")
+    answer = input("When is the first computer invented?: A: 1822, B: 1975, C: 1834, D: 1823")
     if answer = A:
-        print("correct")
+        print("correct, next question")
+        answer = input("What is the most important mechanic in Honkai Star Rail?: A: Gacha system, B: exploring, C: level up, D: IDK ")
+        if answer = A:
+            print("correct")
+            answer = input("How to output onto the screen in python? A: input(), B: OUTPUT, C: print(), D: pintt()")
+            if answer = C:
+                print("correct")
+                answer = input("Characteristic of living organism represented by S?: A: Reproduction, B: Sensitivity, C: Excretion, D: Growth")
+                if answer = B:
+                    print("correct")
+                else:
+                    answer = "Wrong"
+            else:
+            answer = "Wrong"
+        else:
+            answer = "Wrong"
     else:
         answer = "Wrong"
-    
