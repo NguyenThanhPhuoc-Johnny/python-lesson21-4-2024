27c253ac4060c3addf3bc458764effdd13e8ad0e
NguyenThanhPhuoc-Johnny <164726936+NguyenThanhPhuoc-Johnny@users.noreply.github.com>
2024-04-21 15:00:20 +0700

    Update and rename bai4.html to bai4.py

---
+++ bai4.py
@@ -0,0 +1,8 @@
+answer = "Correct"
+while answer:
+    answer = input("When is the first computer invented?: \n A: 1822 \n B: 1975 \n C: 1834 \n D: 1823")
+    if answer = A:
+        print("correct")
+    else:
+        answer = "Wrong"
+    
